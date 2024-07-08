2f8424bf0f63f243c4b1aa5f5d826923e5c73d6a
l0uise <louisepop14@gmail.com>
2024-07-08 19:59:22 +0200

    étape 2: importer les datas

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,29 +1,12 @@
     async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+        try {
+            const response = await fetch('data/photographers.json'); //chemin fichier JSON
+            const data = await response.json(); //converti la réponse en un objet javascript
+            //console.log(data);
+            return data;
+        } catch (error) {
+            console.error('Erreur lors de la récupération des données:', error);
+        }
     }
 
     async function displayData(photographers) {
@@ -37,7 +20,7 @@
     }
 
     async function init() {
-        // Récupère les datas des photographes
+        //récupère les datas des photographes
         const { photographers } = await getPhotographers();
         displayData(photographers);
     }
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,48 @@
+/**
+ * la fonction photographerTemplate prend un argument data et utilise 
+ * la déstructuration pour extraire les propriétés name, portrait, city, country, tagline, price, et id de l'objet
+ * @param {*} data 
+ * @returns name, picture, getUserCardDOM
+ */
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price, id } = data;
 
+    //la var picture est définie pour contenir le chemin de l'image du photographe
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+        //création élément article qui contiendra toutes les informations sur le photographe
+        const article = document.createElement('article');
+
+        //picture
+        const pImg = document.createElement('img');
+        pImg.setAttribute("src", picture);
+        pImg.setAttribute("alt", name);
+
+        //name
+        const pName = document.createElement('h2');
+        pName.textContent = name;
+
+        //city
+        const pCity = document.createElement('p');
+        pCity.textContent = `${city}, ${country}`;
+
+        //tagline
+        const pTagline = document.createElement('p');
+        pTagline.textContent = tagline;
+
+        //price
+        const pPrice = document.createElement('p');
+        pPrice.textContent = `${price}€/jour`;
+
+        //les éléments créés sont ajoutés comme enfants de l'élément article
+        article.appendChild(pImg);
+        article.appendChild(pName);
+        article.appendChild(pCity);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
+
+        return article;
     }
-    return { name, picture, getUserCardDOM }
+    return { name, picture, getUserCardDOM };
 }

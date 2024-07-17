ca9725bea257326781e9631e6d0234bf4a085d5d
l0uise <louisepop14@gmail.com>
2024-07-17 21:33:06 +0200

    étape 6: créer la modale de contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,26 @@
+const modal = document.getElementById("contact_modal");
+const backgroundModal = document.querySelector(".background-modal");
+
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
+    backgroundModal.style.display = "block";
 	modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
+    backgroundModal.style.display = "none";
     modal.style.display = "none";
 }
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const prenom = document.getElementById('prenom').value;
+    const nom = document.getElementById('nom').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    console.log('Prénom:', prenom);
+    console.log('Nom:', nom);
+    console.log('Email:', email);
+    console.log('Message:', message);
+});

4691ce48f233770683fffd0a25ab7c5a62e09e69
l0uise <louisepop14@gmail.com>
2024-07-17 17:27:49 +0200

    étape 3: intégrer la page d'accueil

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -24,6 +24,43 @@
         const { photographers } = await getPhotographers();
         displayData(photographers);
     }
+
+    
+
+     /**
+     * permet de naviguer entre les articles grâce aux touches du clavier
+     * @param {*} event 
+     */
+     function handleKeyDown(event) {
+        //const firstElement = document.querySelector('[tabindex="0"]');
+        const activeElement = document.activeElement;
+
+            //si la touche pressée est la flèche du bas ou la flèche de droite 
+            if (event.key === 'ArrowDown' || event.key === 'ArrowRight') {
+
+                const nextElement = activeElement.nextElementSibling;
+
+                if (nextElement && nextElement.tagName.toLowerCase() === 'article') {
+                    nextElement.focus();
+                }
+            //si la touche pressée est la flèche du haut ou la flèche de gauche
+            } else if (event.key === 'ArrowUp' || event.key === 'ArrowLeft') {
+
+                const prevElement = activeElement.previousElementSibling;
+
+                if (prevElement && prevElement.tagName.toLowerCase() === 'article') {
+                    prevElement.focus();
+                }
+            }
+        
+        
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
     
     init();
+
+  
+
+    
     

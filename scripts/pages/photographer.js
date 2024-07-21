2d4d46b7259f9cd9745d5af30b8298183ccc8b8a
l0uise <louisepop14@gmail.com>
2024-07-21 12:06:49 +0200

    étape 8: afficher et gérer les likes

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -22,6 +22,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     //récupère les médias du photographe correspondant
     const photographerMedias = photographersData.media.filter((media) => 
         Number(id) === Number(media.photographerId));
+
+    //calcule le nombre total de like d'un photographe
+    let totalLikes = photographerMedias.reduce((sum, media) => sum + media.likes, 0);
     
     //affiche les informations du photographe
     function displayPhotographerInfo(photographer) {
@@ -29,23 +32,36 @@ document.addEventListener("DOMContentLoaded", async () => {
         const photographerInformations = document.querySelector(".photograph-informations");
         const photographerTarif = document.querySelector(".photograph-tarif");
         
+        //name
         const name = document.createElement('h1');
         name.textContent = photographer.name;
         name.setAttribute('tabindex', '0');
         
+        //location
         const location = document.createElement('span');
         location.textContent = `${photographer.city}, ${photographer.country}`;
         location.setAttribute('tabindex', '0');
 
+        //tagline
         const tagline = document.createElement('p');
         tagline.textContent = photographer.tagline;
         tagline.setAttribute('tabindex', '0');
         
+        //pp
         const img = document.createElement('img');
         img.setAttribute('src', `assets/photographers/${photographer.portrait}`);
         img.setAttribute('alt', photographer.name);
         img.setAttribute('tabindex', '0');
 
+        //likes : nombre de likes et image like
+        const likes = document.createElement("p");
+        //ajout d'un id pour le total des likes
+        likes.setAttribute('id', 'total-likes'); 
+        likes.textContent = `${totalLikes}`;
+        const likeImg = document.createElement("img");
+        likeImg.setAttribute('src', `assets/icons/heartB.svg`);
+
+        //tarif
         const tarif = document.createElement("p");
         tarif.textContent = `${photographer.price}€ / jour`;
 
@@ -53,6 +69,8 @@ document.addEventListener("DOMContentLoaded", async () => {
         photographerInformations.appendChild(location);
         photographerInformations.appendChild(tagline);
         photographerHeader.appendChild(img);
+        photographerTarif.appendChild(likes);
+        photographerTarif.appendChild(likeImg);
         photographerTarif.appendChild(tarif);
     }
 
@@ -65,7 +83,7 @@ document.addEventListener("DOMContentLoaded", async () => {
             mediaContent = document.createElement('img');
             mediaContent.setAttribute('src', `assets/medias/${media.photographerId}/${media.image}`);
             mediaContent.setAttribute('alt', media.title);
-            mediaContent.setAttribute('tabindex', '0');
+            mediaContent.setAttribute('tabindex', '0'); 
             mediaContent.setAttribute('data-index', index); //data-index pour naviguer entre les images de la lightbox
         } 
         //si le média est une vidéo
@@ -89,29 +107,82 @@ document.addEventListener("DOMContentLoaded", async () => {
 
     //affiche les médias du photographe
     function displayPhotographerMedias(medias) {
-
+        //bloc div media section
         const mediaSection = document.querySelector(".media-section");
 
         medias.forEach((media, index) => {
+            //bloc pour un média
             const mediaBloc = document.createElement('div');
             mediaSection.appendChild(mediaBloc);
 
+            //image/vidéo
             const mediaElement = mediaFactory(media, index);
             mediaBloc.appendChild(mediaElement);
 
+            //information du média : div qui contient titre et nombre de like
+            //bloc
+            const mediaInformations = document.createElement('div');
+            mediaInformations.classList.add('media-information');
+
+            //titre
             const mediaTilte = document.createElement('h1');
             mediaTilte.textContent = media.title;
-            mediaBloc.appendChild(mediaTilte);
 
-            //event listener pour ouvrir lightbox et affiché le média correspondant
+            //nombre de like
+            const numberLike = document.createElement('h1');
+            numberLike.textContent = `${media.likes}`;
+            numberLike.classList.add('media-like');
+            numberLike.setAttribute('data-liked', 'false');
+
+            //image heart like
+            const mediaLikeImg = document.createElement('img');
+            mediaLikeImg.setAttribute('src', `assets/icons/heart.svg`);
+
+
+            mediaInformations.appendChild(mediaTilte);
+            mediaInformations.appendChild(numberLike);
+            mediaInformations.appendChild(mediaLikeImg);
+            mediaInformations.style.display = "flex";
+            mediaBloc.appendChild(mediaInformations);
+
+            //event listener clic sur img heart pour liker
+            mediaLikeImg.addEventListener('click', () => updateLike(numberLike, mediaLikeImg));
+
+            //event listener pour ouvrir lightbox et afficher le média correspondant
             mediaElement.addEventListener('click', () => openLightbox(index)); 
             mediaElement.addEventListener('keydown', function(event) {
                 if (event.key === 'Enter') {
-                    openLightbox(index)
+                    openLightbox(index);
                 }
             });
 
         });
+    }
+
+    //incrémente/décrémente les likes
+    function updateLike(mediaLikeElement, mediaLikeImg) {
+        const isLiked = mediaLikeElement.getAttribute('data-liked') === 'true';
+
+        //on récupère le nombre de like en txt qu'on met en int
+        let likesCount = parseInt(mediaLikeElement.textContent);
+
+        if (isLiked) {
+            likesCount--; //décrémente le nombre de likes d'une image
+            mediaLikeElement.setAttribute('data-liked', 'false');
+            mediaLikeImg.classList.remove('liked'); //retire la classe liked
+            totalLikes--; //décrémente le nombre total des likes
+        } else {
+            likesCount++;//incrémente le nombre de likes d'une image
+            mediaLikeElement.setAttribute('data-liked', 'true');
+            mediaLikeImg.classList.add('liked'); //retire la classe liked
+            totalLikes++; //incrémente le nombre total des likes
+        }
+
+        mediaLikeElement.textContent = likesCount;
+
+        //met à jour le total des likes affiché
+        const totalLikesElement = document.getElementById('total-likes');
+        totalLikesElement.textContent = `${totalLikes}`;
     }
 
     //ouvre la lightbox et affiche le média sélectionné

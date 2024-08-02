d2e483087fdd6b16ca02695de20db4a5dc559b33
✿louise <100213278+uu-0@users.noreply.github.com>
2024-08-02 19:05:41 +0200

    ajout commentaire index.js pour supprimer erreur eslint

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -12,7 +12,8 @@
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
 
-        photographers.forEach((photographer) => {
+        photographers.forEach((
+            // eslint-disable-next-line
             const photographerModel = photographerTemplate(photographer);
             const userCardDOM = photographerModel.getUserCardDOM();
             photographersSection.appendChild(userCardDOM);
